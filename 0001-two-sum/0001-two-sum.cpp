31af1d57abf5de811cd11ad68d21323793a36ba4
Anchal <74677360+ANCHAL628@users.noreply.github.com>
2024-05-08 21:24:43 +0530

    Time: 104 ms (17.28%), Space: 12.5 MB (92.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int,int>mp ;
-        vector<int>answer ;
-       for(int i = 0 ; i < nums.size() ; i++) {
-           if(mp.find(target-nums[i]) != mp.end()){
-              answer.push_back(i) ;
-              answer.push_back(mp[target-nums[i]]) ;
-           }
-           mp[nums[i]] = i ;
-       }
-        return answer ;
-        ///
+       // unordered_map<int,int>mp ;
+       //  vector<int>answer ;
+       // for(int i = 0 ; i < nums.size() ; i++) {
+       //     if(mp.find(target-nums[i]) != mp.end()){
+       //        answer.push_back(i) ;
+       //        answer.push_back(mp[target-nums[i]]) ;
+       //     }
+       //     mp[nums[i]] = i ;
+       // }
+       //  return answer ;
+       //  ///
+        vector<int>ans ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            int answer = 0 ;
+            for(int j = i+1 ; j < nums.size() ; j++){
+                answer = nums[i]+nums[j] ;
+                if(answer == target){
+                    ans.push_back(j) ;
+                    ans.push_back(i) ;
+                    return ans ;
+                }
+            }
+        }
+        return ans ;
     }
 };
 //tc : O(n)
