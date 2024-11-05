fcb8b18b06cbeda2b049f116e9bb3eaf196e54a2
Anchal . <74677360+ANCHAL628@users.noreply.github.com>
2024-11-05 17:44:08 +0530

    Time: 0 ms (100.00%), Space: 27 MB (79.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>mp ;
-       int n = nums.size() ;
-       for(auto it : nums){
-           mp[it]++ ;
-           if(mp[it] > n/2){
-               return it ;
-           }
-       }
-        return 0 ;
+     //n^2 element type
+     //use hashmap and count the no of timnes a particular element occur 
+        //moore voting algorithm
+     int n = nums.size()-1 ;
+     int candidate = nums[0] ;
+     int count = 1 ;
+     for(int i = 1 ; i < nums.size() ; i++){
+        if(candidate == nums[i]) {
+            count++ ;
+        }else{
+            count-- ;
+            if(count == 0){
+                candidate = nums[i] ;
+                count = 1 ;
+            }
+        }
+      }
+      return candidate ;
     }
 };
